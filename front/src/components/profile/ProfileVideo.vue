c487241e6b41f38ae2fd55983ff8afe6a649400a
Franz Ka <koehler.francois@gmail.com>
2024-09-02 15:29:48 -0700

    lecteur video

--- front/src/components/profile/ProfileVideo.vue
+++ front/src/components/profile/ProfileVideo.vue
@@ -1,3 +1,19 @@
-<template>ici video</template>
+<template>
+  <div class="profile-video">
+    <vue-plyr>
+      <div class="plyr__video-embed">
+        <iframe
+          src="https://youtube-nocookie.com/embed/ZTeqM5gciH8"
+          allowfullscreen
+          allowtransparency
+          allow="autoplay"
+        >
+        </iframe>
+      </div>
+    </vue-plyr>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// https://youtube-nocookie.com/embed/ZTeqM5gciH8
+</script>
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
-import App from './App.vue'
-import '@/assets/scss/_styles.scss'
-
+import VuePlyr from 'vue-plyr'
 import router from './router'
+import App from './App.vue'
+
+import '@/assets/scss/_styles.scss'
+import 'vue-plyr/dist/vue-plyr.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VuePlyr)
 
 app.mount('#app')
