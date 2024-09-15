0f24856becea61f31271246e3e05d9a24a4451d7
Franz Ka <koehler.francois@gmail.com>
2024-09-15 16:22:09 -0700

    menu en picto wip

--- front/src/components/profile/ProfileNavBar.vue
+++ front/src/components/profile/ProfileNavBar.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="chips-menu">
-    <RouterLink :to="{ name: 'profile-home' }">Home</RouterLink>
-    <RouterLink :to="{ name: 'profile-pictures' }">Pictures</RouterLink>
-    <RouterLink :to="{ name: 'profile-videos' }">Videos</RouterLink>
-    <RouterLink :to="{ name: 'profile-music' }">Music</RouterLink>
-    <RouterLink :to="{ name: 'profile-links' }">Links</RouterLink>
+    <RouterLink :to="{ name: 'profile-home' }"><IconHome /></RouterLink>
+    <RouterLink :to="{ name: 'profile-pictures' }">P</RouterLink>
+    <RouterLink :to="{ name: 'profile-videos' }"><IconPlayCircle /></RouterLink>
+    <RouterLink :to="{ name: 'profile-music' }">M</RouterLink>
+    <RouterLink :to="{ name: 'profile-links' }">L</RouterLink>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import IconHome from '@/components/icons/IconHome.vue'
+import IconPlayCircle from '@/components/icons/IconPlayCircle.vue'
+</script>
 
 <style lang="scss" scoped>
 .chips-menu {
