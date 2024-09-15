0b5a5d8324c4217738544a456af43493e6ceeabd
Franz Ka <koehler.francois@gmail.com>
2024-09-15 08:30:02 -0700

    pictures store WIP

--- front/src/views/profile/ProfilePicturesView.vue
+++ front/src/views/profile/ProfilePicturesView.vue
@@ -1,3 +1,14 @@
-<template>Pictures</template>
+<template>
+  Pictures
+  {{ route.params.id }}
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+// import { UsePicturesStore } from '@/stores/picturesStore'
+
+const route = useRoute()
+// const picturesStore = UsePicturesStore()
+
+// picturesStore.fetchPictures(route.params.id)
+</script>
